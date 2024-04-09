10909e6c8c8106f2145f93aebf0a8eb04e399d2c
lcvargas <Luis.Mercon@criticalsoftware.com>
2024-04-09 19:22:22 +0100

    first commit

---
+++ backend/pdmovies/src/main/java/pt/isec/pdmovies/pdmovies/PdmoviesApplication.java
@@ -0,0 +1,13 @@
+package pt.isec.pdmovies.pdmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdmoviesApplication.class, args);
+	}
+
+}
---
+++ backend/pdmovies/src/test/java/pt/isec/pdmovies/pdmovies/PdmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.isec.pdmovies.pdmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

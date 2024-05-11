5482af8d5c6bbb2ee8a74f81693699237ddd3187
Artur Shamrin <36150765+arturheath@users.noreply.github.com>
2024-05-11 18:35:35 +0800

    Update vite.config.js

--- frontend/pd-movies-presentation/vite.config.js
+++ frontend/pd-movies-presentation/vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Explicitly specifying the build output directory
+  },
   server: {
-    port: 5000  // Set the port explicitly
+    port: 5000  // Ensuring the development server port
   }
 });

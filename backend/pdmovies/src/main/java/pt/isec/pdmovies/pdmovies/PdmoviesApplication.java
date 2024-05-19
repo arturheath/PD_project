cbdcb045a5f009e3392646f0b93eb5e7d902ae75
Artur Shamrin <36150765+arturheath@users.noreply.github.com>
2024-05-19 22:49:36 +0800

    testing jenkins trigger

--- backend/pdmovies/src/main/java/pt/isec/pdmovies/pdmovies/PdmoviesApplication.java
+++ backend/pdmovies/src/main/java/pt/isec/pdmovies/pdmovies/PdmoviesApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PdmoviesApplication {
     public static void main(String[] args) {
+        //yo
         SpringApplication.run(PdmoviesApplication.class, args);
     }
 

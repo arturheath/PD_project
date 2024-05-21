ad7417e5fc6ea2bbd82970ebdc1ca0f14fdd30d8
Artur Shamrin <36150765+arturheath@users.noreply.github.com>
2024-05-22 02:26:01 +0800

    Update PdmoviesApplication.java

--- backend/pdmovies/src/main/java/pt/isec/pdmovies/pdmovies/PdmoviesApplication.java
+++ backend/pdmovies/src/main/java/pt/isec/pdmovies/pdmovies/PdmoviesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PdmoviesApplication {
     public static void main(String[] args) {
-    
+    //comment
         SpringApplication.run(PdmoviesApplication.class, args);
     }
 
